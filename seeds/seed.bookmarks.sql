0274cecb4e74f9fb8176d1de3b8376de4a281757
AdrianM <30712077+amichot@users.noreply.github.com>
2019-05-22 16:28:55 -0500

    added db bookmarks and testbookmarks

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,15 @@
+
+
+INSERT INTO bookmarks (title, url, description, rating)
+VALUES
+	('Thinkful', 'https:\/\/www.thinkful.com', 'Think outside the classroom', '5'),
+	('Google', 'https:\/\/www.google.com', 'Where we find everything else', '4'),
+	('MDN', 'https:\/\/www.developer.mozilla.org', 'The only place to find web documentation', '5'),
+	('NFL', 'https:\/\/www.nfl.com', 'Professional American Football League', '4'),
+	('MLB', 'https:\/\/www.mlb.com', 'Professional Baseball League', '3'),
+	('Youtube', 'https:\/\/www.youtube.com', 'videos, music, entertainment', '5'),
+	('NBA', 'https:\/\/www.nba.com', 'Professional Basketball League', '2'),
+	('Trading View', 'https:\/\/www.tradingview.com','stock market charts', '3'),
+	('Linkedin', 'https:\/\/www.linkedin.com', 'Largest professional network', '4'),
+	('Yahoo', 'https:\/\/www.yahoo.com', 'search engine', '2')
+
